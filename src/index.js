0c430f48a5a66377f48f75e63b2356bae5b41ff2
Bahdanovich Yauheni <bahdanovich.yauheni@gmail.com>
2020-03-21 15:49:23 +0300

    init: typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(arr){
+    if (!arr || arr.length === 0){return 0;}
+    let min = arr[0];
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] < min){
+            min = arr[i];
+        }
+    }
+    return min
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(arr){
+    if (!arr || arr.length === 0){return 0;}
+    let max = arr[0];
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] > max){
+            max = arr[i];
+        }
+    }
+    return max
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(arr){
+    let avg = 0;
+    let sum = 0;
+    if (!arr || arr.length === 0){return 0;}
+    for (let i = 0; i < arr.length; i++){
+        sum = sum + arr[i];
+    }
+    avg = sum / arr.length;
+
+
+    return avg
 }
